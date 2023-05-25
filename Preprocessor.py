01b69dd3adac719e80d6ffb8e1223cde29a3939b
Tom Saveur <t.saveur@student.tudelft.nl>
2023-05-25 21:44:36 +0200

    Add more emojis

--- Preprocessor.py
+++ Preprocessor.py
@@ -12,10 +12,18 @@ newline_pattern = '\n'
 special_pattern = '[#\'\"]'
 
 emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # Emoticons
-                           u"\U0001F300-\U0001F5FF"  # Symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # Transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # Flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           u"\U0001F1E0-\U0001F1FF"
+                           u"\U0001F300-\U0001F5FF"
+                           u"\U0001F600-\U0001F64F"
+                           u"\U0001F680-\U0001F6FF"
+                           u"\U0001F700-\U0001F77F"
+                           u"\U0001F780-\U0001F7FF"
+                           u"\U0001F800-\U0001F8FF"
+                           u"\U0001F900-\U0001F9FF"
+                           u"\U0001FA00-\U0001FA6F"
+                           u"\U0001FA70-\U0001FAFF"
                            "]+", flags=re.UNICODE)
 
 generic_re = re.compile('|'.join([at_pattern, tag_pattern, url_pattern, newline_pattern, special_pattern]))
@@ -45,7 +53,6 @@ class Preprocessor:
 
         # Count the words, and only keep relevant text by removing short tweets.
         min_count = 3
-        # self.data['Word_Count'] = self.data['Clean'].str.split(' ').str.len()
         self.data = self.data[self.data['Clean'].str.split(' ').str.len() > min_count]
 
         print('Done Checking Word Count')
