27c1d4388d379be8caddf7f2aef17b2a8e04c7bb
Tom Saveur <t.saveur@student.tudelft.nl>
2023-05-26 12:32:23 +0200

    Add model creation and saving

---
+++ Model.py
@@ -0,0 +1,39 @@
+"""
+This module allows for the modeling of the data, which can later be used for predicting important topics.
+"""
+import joblib
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+import config
+
+
+class TopicModel:
+    """
+    This class creates a Model based on the data provided by Ciphix.
+    """
+    def __init__(self, df, topic_count=10):
+        self.df = df
+        self.topic_count = topic_count
+        self.nmf = NMF(n_components=self.topic_count)
+        self.vector = CountVectorizer(min_df=5, max_df=0.9, stop_words='english')
+
+        self.data_vector = self.vector.fit_transform(self.df['Preprocessed'])
+        self.data_nmf = self.nmf.fit_transform(self.data_vector)
+
+    def find_topics(self):
+        res = []
+        for topic in self.nmf.components_:
+            # Sort the topics in reversed order to get the most important first
+            reverse_sorted = topic.argsort()[::-1][:self.topic_count]
+            # Map to the correct words from the vector
+            important_features = [self.vector.get_feature_names_out()[i] for i in reverse_sorted]
+
+            # Add the string to the final result
+            res.append(' '.join(important_features))
+        print(res)
+        return res
+
+    def save_model(self):
+        joblib.dump(self.nmf, f'{config.ROOT_DIR}\\models\\nmf.joblib')
+        joblib.dump(self.vector, f'{config.ROOT_DIR}\\models\\vectorizer.joblib')
